990425258d9c71897cd5c4e6d1bed8be71c9ca06
rudolfrada <rudolfrada@users.noreply.github.com>
2023-11-25 12:36:49 +0100

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showNumber(1)
+    basic.pause(500)
+    basic.showNumber(2)
+    basic.pause(500)
+    basic.showNumber(3)
+    basic.pause(500)
+    basic.showNumber(4)
+    basic.pause(500)
+    basic.showNumber(5)
+    basic.pause(500)
+})
